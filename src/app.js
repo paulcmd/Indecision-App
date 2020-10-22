43330a40b78799a5e05df8b7df44fd9e368b2657
paulcmd <choxmail.pm@gmail.com>
2020-10-22 05:10:21 -0700

    Introducing state to components

--- src/app.js
+++ src/app.js
@@ -1,14 +1,19 @@
 class IndecisionApp extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			options: ['1','2','3']
+		}
+	}
 	render() {
 		const title = 'Indecision';
 		const subtitle = 'Put your life in the hands of a computer';
-		const options = [];
 
 		return (
 			<div>
 				<Header title={title} subtitle={subtitle} />
 				<Action />
-				<Options options={options} />
+				<Options options={this.state.options} />
 				<AddOption />
 			</div>
 		);
