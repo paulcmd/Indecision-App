74c529e50f22b6ac82fb5700ee1a16902e1dcd1c
paulcmd <choxmail.pm@gmail.com>
2020-11-02 13:21:12 -0700

    Styling Components

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -6,10 +6,12 @@ const OptionModal = (props) => (
         isOpen={!!props.selectedOption}
         contentLabel="Selected Option" //accessibility option
         onRequestClose={props.handleDeleteModalOption} //removes modal when press escape or click outside modal
+        closeTimeoutMS={300}
+        className="modal"
     >
-        <h3>Selected Option</h3>
-        {props.selectedOption && <p>{props.selectedOption}</p>}
-        <button onClick={props.handleDeleteModalOption}>Okay</button>
+        <h3 className="modal__title">Selected Option</h3>
+        {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+        <button className="button" onClick={props.handleDeleteModalOption}>Okay</button>
     </Modal>
 )
 
